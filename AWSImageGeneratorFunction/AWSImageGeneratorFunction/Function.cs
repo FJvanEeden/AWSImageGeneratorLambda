3edd9c1e776fb8211ae1439412f0908cb051f7b9
FJvanEeden <fredz.eeden@gmail.com>
2020-05-12 09:10:18 +0200

    Function now publishes to SNS.

--- AWSImageGeneratorFunction/AWSImageGeneratorFunction/Function.cs
+++ AWSImageGeneratorFunction/AWSImageGeneratorFunction/Function.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -22,15 +24,31 @@ namespace AWSImageGeneratorFunction
       }
 
       Console.WriteLine("Processing complete.");
+      
       return $"Processed {evnt.Records.Count} records.";
     }
 
     private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
     {
-      context.Logger.LogLine($"Processed message {message.MessageId}");
+      context.Logger.LogLine($"Processed message {message.Body}");
 
-      // TODO: Do interesting work based on the new message
+      //
+      await PublishToSNS();
       await Task.CompletedTask;
+    }
+
+    private async Task PublishToSNS()
+    {
+      string message = "Test at {DateTime.UtcNow.ToLongDateString()}";
+      var client = new AmazonSimpleNotificationServiceClient(region: Amazon.RegionEndpoint.USWest2);
+
+      var testRequest = new PublishRequest
+      {
+        Message = message,
+        TopicArn = "arn:aws:sns:us-west-2:494051244674:example-sns-receiver",
+      };
+
+      var response = await client.PublishAsync(testRequest);
     }
   }
 }
